d74ce14bed955451a94044a48c591641dbd111db
michele.mongardi2 <michele.mongardi2@studio.unibo.it>
2021-06-28 12:50:57 +0200

    removed unused import

--- src/main/java/PDFAnalyzerActors/Actors/AnalyzerMain.java
+++ src/main/java/PDFAnalyzerActors/Actors/AnalyzerMain.java
@@ -1,6 +1,5 @@
 package PDFAnalyzerActors.Actors;
 import akka.actor.typed.ActorRef;
-import akka.actor.typed.ActorSystem;
 import akka.actor.typed.Behavior;
 import akka.actor.typed.javadsl.AbstractBehavior;
 import akka.actor.typed.javadsl.ActorContext;
--- src/main/java/PDFAnalyzerActors/Actors/Generator.java
+++ src/main/java/PDFAnalyzerActors/Actors/Generator.java
@@ -13,7 +13,6 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Stream;
 
